02758c37c2f02990bdc6613d734f9ffcdddf265a
Alexey Zhokhov <alexey@zhokhov.com>
2018-08-10 06:26:14 +0800

    Use underscore.

--- env.sh
+++ env.sh
@@ -5,6 +5,6 @@ export NGINX_VERSION="1.15.2"
 # https://github.com/apache/incubator-pagespeed-mod/releases
 export MOD_PAGESPEED_VERSION="1.13.35.2"
 
-export VERSION="${NGINX_VERSION}-${MOD_PAGESPEED_VERSION}-alpine"
+export VERSION="${MOD_PAGESPEED_VERSION}_${NGINX_VERSION}-alpine"
 
 export DOCKER_REPOSITORY="donbeave/nginx-pagespeed-psol"
